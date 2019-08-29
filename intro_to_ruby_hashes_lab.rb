aab6a648a8d0a233871478ffcbeadf1be9bf7fab
Micah Levine <micah.levine@outlook.com>
2019-08-29 01:13:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,7 +48,7 @@ def monopoly_with_fourth_tier
 		names: {
 			reading_railroad: {},
 			pennsylvania_railroad: {
-				mortgage_value: $100
+				"mortgage_value": "$100"
 			},
 			b_and_o_railroad: {},
 			shortline: {}
